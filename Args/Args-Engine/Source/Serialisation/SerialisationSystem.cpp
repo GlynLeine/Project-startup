f08a0f82396df376c8a7a83e9d971d9aee2183d8
ragingram2 <blazinram@gmail.com>
2020-02-10 18:50:55 +0100

    More JSON testing
    Put on pause for higher priority features

--- Args/Args-Engine/Source/Serialisation/SerialisationSystem.cpp
+++ Args/Args-Engine/Source/Serialisation/SerialisationSystem.cpp
@@ -1,19 +1,44 @@
 #include "Serialisation/SerialisationSystem.h"
 #include "rapidjson/rapidjson.h"
 #include "rapidjson/document.h"
+#include <fstream>
+#include <iostream>
+#include <array>
+#include <list>
 
 using namespace rapidjson;
 
 void SerialisationSystem::Init()
 {
 
+	fstream inFile;
+	std::string json;
+	inFile.open("C:/Users/blazi/Documents/Fork/Project-startup/Args/Args-Editor/Json Scene Serializer/Assets/SampleScene.JSON");
+	if (!inFile) {
+		cout << "Unable to open file";
+		exit(1); // terminate with error
+	}
+	if (inFile.is_open())
+	{
+		getline(inFile, json);
+	}
+	inFile.close();
 	Debug::Log(DebugInfo, "Init SerialisationSystem");
 	Document document;
-	document.Parse("{\"name\" : \"Rowan\",\"age\":19}");
-	assert(document.HasMember("name"));
-	assert(document.HasMember("age"));
-	assert(document["name"].IsString());
-	assert(document["age"].IsInt());
-	Debug::Log(DebugInfo,document["name"].GetString());
-	Debug::Log(DebugInfo, "%i", document["age"].GetInt());
+	document.Parse(json.c_str());
+	const Value& Scene = document["Scene"];
+	assert(Scene.IsArray());
+	for (SizeType i = 0; i < Scene.Size(); i++) // Uses SizeType instead of size_t
+	{
+		Debug::Log(DebugInfo,Scene[i]["name"].GetString());
+		const Value& components = Scene[i]["components"];
+		assert(components.IsArray());
+		for (SizeType j = 0; j < components.Size();j++)
+		{
+			assert(components[i]["name"].IsString());
+			//Debug::Log(DebugInfo, "Component:"components[i]["name"].GetString());
+		}
+		//Debug::Log(DebugInfo, Scene[i]["name"].GetString());
+		
+	}
 }
