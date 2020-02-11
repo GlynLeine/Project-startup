41e9c9500f1700bdf0133fa9c60d273d68b0a85b
Arjen de Aldrey <adealdrey3@gmail.com>
2020-02-11 15:00:05 +0100

    [ADD] Unity Prototype

---
+++ Args/Args-Editor/Project Protos/Assets/Movement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float speed;
+
+    [SerializeField] private float rotateSpeed;
+    private PickUp pickUp;
+    [SerializeField] private float JumpSpeed;
+    private float angle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pickUp = GetComponent<PickUp>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.up, -rotateSpeed);
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up, rotateSpeed);
+        }
+
+        
+
+        if (!pickUp.pickup)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                transform.GetComponent<Rigidbody>().AddForce(Vector3.up * JumpSpeed);
+            }
+            if (Input.GetKey(KeyCode.W) && !pickUp.inFront)
+            {
+                transform.position += transform.forward * Time.deltaTime * speed;
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                transform.position -= transform.forward * Time.deltaTime * speed;
+            }
+        }
+
+
+    }
+}
---
+++ Args/Args-Editor/Project Protos/Assets/Movement2.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement2 : MonoBehaviour
+{
+    [SerializeField] float speed;
+
+    [SerializeField] private float rotateSpeed;
+    private Push pushScript;
+    [SerializeField] private float JumpSpeed;
+    private float angle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pushScript = GetComponent<Push>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(pushScript.Pushing);
+        if (!pushScript.Pushing)
+        {
+            if (Input.GetKeyDown(KeyCode.RightShift))
+            {
+                transform.GetComponent<Rigidbody>().AddForce(Vector3.up * JumpSpeed);
+            }
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.Rotate(Vector3.up, -rotateSpeed);
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.Rotate(Vector3.up, rotateSpeed);
+            }
+        }
+
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += transform.forward * Time.deltaTime * speed;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position -= transform.forward * Time.deltaTime * speed;
+        }
+
+    }
+}
